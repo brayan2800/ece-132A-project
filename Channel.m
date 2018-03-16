d5d67b886823e4c138cee47bdd5744a71d843aa2
floresjd <28540828+floresjd@users.noreply.github.com>
2018-03-15 20:41:32 -0700

    Changed Implementation
    flip_bits: (takes a codeword in vector form, takes a mask in dec form)
       returns a decimal that represents the bits flipped

--- Channel.m
+++ Channel.m
@@ -1,52 +1,11 @@
 classdef Channel
     methods (Static)
-        function corrupt = flip_one(cw,which)
-            codeword = Helper.make_row(cw);
-            if codeword(1,which) == 1
-                codeword(1,which) = 0;
-            else %corrupt(which) == 0
-                codeword(1,which) = 1;
-            end
-            corrupt = zeros(2,5);
-            corrupt(1,1:5) = codeword(1:5);
-            corrupt(2,1:5) = codeword(6:10);
-        end
-        function corrupt = flip_two(cw,w1,w2)
-            codeword = Helper.make_row(cw);
-            if codeword(1,w1) == 1
-                codeword(1,w1) = 0;
-            else %corrupt(w1) == 0
-                codeword(1,w1) = 1;
-            end
-            if codeword(1,w2) == 1
-                codeword(1,w2) = 0;
-            else %corrupt(w1) == 0
-                codeword(1,w2) = 1;
-            end
-            corrupt = zeros(2,5);
-            corrupt(1,1:5) = codeword(1:5);
-            corrupt(2,1:5) = codeword(6:10);
-        end
-        function corrupt = flip_three(cw,w1,w2,w3)
-            codeword = Helper.make_row(cw);
-            if codeword(1,w1) == 1
-                codeword(1,w1) = 0;
-            else %corrupt(w1) == 0
-                codeword(1,w1) = 1;
-            end
-            if codeword(1,w2) == 1
-                codeword(1,w2) = 0;
-            else %corrupt(w1) == 0
-                codeword(1,w2) = 1;
-            end
-            if codeword(1,w3) == 1
-                codeword(1,w3) = 0;
-            else %corrupt(w3) == 0
-                codeword(1,w3) = 1;
-            end
-            corrupt = zeros(2,5);
-            corrupt(1,1:5) = codeword(1:5);
-            corrupt(2,1:5) = codeword(6:10);
+        function corrupt = flip_bits(cw,mask)
+            int = uint16(Helper.binvec2dec(cw, 10));
+            mask = uint16(mask);
+            bit_flipped = bitand(bitcmp(bitand(int,mask)),mask);
+            clear_bit_flipped = bitand(int,bitcmp(mask));
+            corrupt = bitor(bit_flipped,clear_bit_flipped);
         end
     end
 end
