9c1da7d67a9e9b4f3a4bf6003595aca858f0c784
brayan2800 <brayan2800@gmail.com>
2018-03-15 23:37:53 -0700

    Input can now be of any size

--- VitDec.m
+++ VitDec.m
@@ -1,9 +1,11 @@
 classdef VitDec < handle
     properties
-        path_metrics = zeros(4,6);
+        columns = 0;
+        path_metrics = zeros(4, 6);
+        
         paths1 = zeros(1,2);
         paths2 = zeros(1,4);
-        paths3 = zeros(1,8);
+        paths_full = zeros(1,8);
         paths4 = zeros(1,4);
         paths5 = zeros(1,2); 
         
@@ -14,6 +16,8 @@ classdef VitDec < handle
     end
 	methods
         function fill_trellis(obj, in)
+		   n_full_stages = obj.columns-4;
+           
            %stage 1
            obj.paths1(1) = obj.hamming_distance(obj.x(1:2,1),in(1:2,1));
            obj.paths1(2) = obj.hamming_distance(obj.x(1:2,2),in(1:2,1));
@@ -32,111 +36,117 @@ classdef VitDec < handle
            obj.path_metrics(3,3) = obj.paths2(3) + obj.path_metrics(2,2);
            obj.path_metrics(4,3) = obj.paths2(4) + obj.path_metrics(2,2);
            
-           %Stage 3
-           obj.paths3(1) = obj.hamming_distance(obj.x(1:2,1),in(1:2,3));
-           obj.paths3(2) = obj.hamming_distance(obj.x(1:2,2),in(1:2,3));
-           obj.paths3(3) = obj.hamming_distance(obj.x(1:2,3),in(1:2,3));
-           obj.paths3(4) = obj.hamming_distance(obj.x(1:2,4),in(1:2,3));
-           obj.paths3(5) = obj.hamming_distance(obj.x(1:2,5),in(1:2,3));
-           obj.paths3(6) = obj.hamming_distance(obj.x(1:2,6),in(1:2,3));
-           obj.paths3(7) = obj.hamming_distance(obj.x(1:2,7),in(1:2,3));
-           obj.paths3(8) = obj.hamming_distance(obj.x(1:2,8),in(1:2,3));
-        
-           obj.path_metrics(1,4) = min([obj.paths3(1) + obj.path_metrics(1,3);
-                                    obj.paths3(5) + obj.path_metrics(3,3)]);
-           obj.path_metrics(2,4) = min([obj.paths3(2) + obj.path_metrics(1,3);
-                                    obj.paths3(6) + obj.path_metrics(3,3)]);
-           obj.path_metrics(3,4) = min([obj.paths3(3) + obj.path_metrics(2,3);
-                                    obj.paths3(7) + obj.path_metrics(4,3)]);
-           obj.path_metrics(4,4) = min([obj.paths3(4) + obj.path_metrics(2,3);
-                                    obj.paths3(8) + obj.path_metrics(4,3)]);
-                                
-           %Stage 4
-           obj.paths4(1) = obj.hamming_distance(obj.x(1:2,1),in(1:2,4));
-           obj.paths4(2) = obj.hamming_distance(obj.x(1:2,3),in(1:2,4));
-           obj.paths4(3) = obj.hamming_distance(obj.x(1:2,5),in(1:2,4));
-           obj.paths4(4) = obj.hamming_distance(obj.x(1:2,7),in(1:2,4));
+           %Stages 3- columns-2
+           for i = 1:n_full_stages
+               obj.paths_full(i,1) = obj.hamming_distance(obj.x(1:2,1),in(1:2,i+2));
+               obj.paths_full(i,2) = obj.hamming_distance(obj.x(1:2,2),in(1:2,i+2));
+               obj.paths_full(i,3) = obj.hamming_distance(obj.x(1:2,3),in(1:2,i+2));
+               obj.paths_full(i,4) = obj.hamming_distance(obj.x(1:2,4),in(1:2,i+2));
+               obj.paths_full(i,5) = obj.hamming_distance(obj.x(1:2,5),in(1:2,i+2));
+               obj.paths_full(i,6) = obj.hamming_distance(obj.x(1:2,6),in(1:2,i+2));
+               obj.paths_full(i,7) = obj.hamming_distance(obj.x(1:2,7),in(1:2,i+2));
+               obj.paths_full(i,8) = obj.hamming_distance(obj.x(1:2,8),in(1:2,i+2));
+
+               obj.path_metrics(1,i+3) = min([obj.paths_full(i,1) + obj.path_metrics(1,i+2);
+                                        obj.paths_full(i,5) + obj.path_metrics(3,i+2)]);
+               obj.path_metrics(2,i+3) = min([obj.paths_full(i,2) + obj.path_metrics(1,i+2);
+                                        obj.paths_full(i,6) + obj.path_metrics(3,i+2)]);
+               obj.path_metrics(3,i+3) = min([obj.paths_full(i,3) + obj.path_metrics(2,i+2);
+                                        obj.paths_full(i,7) + obj.path_metrics(4,i+2)]);
+               obj.path_metrics(4,i+3) = min([obj.paths_full(i,4) + obj.path_metrics(2,i+2);
+                                        obj.paths_full(i,8) + obj.path_metrics(4,i+2)]);
+           end 
+           
+           %Second to last stage
+           obj.paths4(1) = obj.hamming_distance(obj.x(1:2,1),in(1:2,obj.columns-1));
+           obj.paths4(2) = obj.hamming_distance(obj.x(1:2,3),in(1:2,obj.columns-1));
+           obj.paths4(3) = obj.hamming_distance(obj.x(1:2,5),in(1:2,obj.columns-1));
+           obj.paths4(4) = obj.hamming_distance(obj.x(1:2,7),in(1:2,obj.columns-1));
            
            
-           obj.path_metrics(1,5) = min([obj.paths4(1) + obj.path_metrics(1,4);
-                                    obj.paths4(3) + obj.path_metrics(3,4)]);
-           obj.path_metrics(3,5) = min([obj.paths4(2) + obj.path_metrics(2,4);
-                                        obj.paths4(4) + obj.path_metrics(4,4)]);
+           obj.path_metrics(1,obj.columns) = min([obj.paths4(1) + obj.path_metrics(1,obj.columns-1);
+                                    obj.paths4(3) + obj.path_metrics(3,obj.columns-1)]);
+           obj.path_metrics(3,obj.columns) = min([obj.paths4(2) + obj.path_metrics(2,obj.columns-1);
+                                        obj.paths4(4) + obj.path_metrics(4,obj.columns-1)]);
            
            
-           %Stage 5
-           obj.paths5(1) = obj.hamming_distance(obj.x(1:2,1),in(1:2,5));
-           obj.paths5(2) = obj.hamming_distance(obj.x(1:2,5),in(1:2,5));
+           %Last Stage
+           obj.paths5(1) = obj.hamming_distance(obj.x(1:2,1),in(1:2,obj.columns));
+           obj.paths5(2) = obj.hamming_distance(obj.x(1:2,5),in(1:2,obj.columns));
            
-           obj.path_metrics(1,6) = min([obj.paths5(1) + obj.path_metrics(1,5);
-                                        obj.paths5(2) + obj.path_metrics(3,5)]);
+           obj.path_metrics(1,obj.columns+1) = min([obj.paths5(1) + obj.path_metrics(1,obj.columns);
+                                        obj.paths5(2) + obj.path_metrics(3,obj.columns)]);
                                     
         end        
    
         function output = traceback(obj)
-            output = zeros(1,5);
-            currstate = [1,6];
+            n_full_stages = obj.columns-4;
+            %output = zeros(1,obj.columns);
+            output(1,1:obj.columns) =  99;
+            currstate = [1,obj.columns+1];
             
-            %stage 5 
-            if  obj.path_metrics(1,6) == obj.path_metrics(1,5)+ obj.paths5(1) 
-               currstate = [1,5];
-               output(5) = 0;
+            %Last Stage 
+            if  obj.path_metrics(1,obj.columns+1) == obj.path_metrics(1,obj.columns)+ obj.paths5(1) 
+               currstate = [1,obj.columns];
+               output(obj.columns) = 0;
             else
-                currstate = [3,5];
-                output(5) = 0;
+                currstate = [3,obj.columns];
+                output(obj.columns) = 0;
             end
             
-            %stage 4
-            if currstate == [1,5]
-                if obj.path_metrics(1,5) == obj.path_metrics(1,4) + obj.paths4(1)
-                   currstate = [1,4];
-                   output(4) = 0;
+            %Second to last stage
+            if currstate == [1,obj.columns]
+                if obj.path_metrics(1,obj.columns) == obj.path_metrics(1,obj.columns-1) + obj.paths4(1)
+                   currstate = [1,obj.columns-1];
+                   output(obj.columns-1) = 0;
                 else
-                    currstate = [3,4];  %if it didn't come from (1,4) then it came from (3,4)
-                    output(4) = 0;
+                    currstate = [3,obj.columns-1];  %if it didn't come from (1,4) then it came from (3,4)
+                    output(obj.columns-1) = 0;
                 end
-            elseif currstate == [3,5]
-                if obj.path_metrics(3,5) == obj.path_metrics(2,4) + obj.paths4(2)
-                   currstate = [2,4];
-                   output(4) = 0;
+            elseif currstate == [3,obj.columns]
+                if obj.path_metrics(3,obj.columns) == obj.path_metrics(2,obj.columns-1) + obj.paths4(2)
+                   currstate = [2,obj.columns-1];
+                   output(obj.columns-1) = 0;
                 else
-                    currstate = [4,4];  %if it didn't come from (2,4) then it came from (4,4)
-                    output(4) = 0;
+                    currstate = [4,obj.columns-1];  %if it didn't come from (2,4) then it came from (4,4)
+                    output(obj.columns-1) = 0;
                 end
             end
             
-            %Stage 3
-            if currstate == [1,4]
-                if obj.path_metrics(1,4) == obj.path_metrics(1,3) + obj.paths3(1)
-                   currstate = [1,3];
-                   output(3) = 0;
-                else
-                    currstate = [3,3];  %if it didn't come from (1,3) then it came from (3,3)
-                    output(3) = 0;
-                end
-            elseif currstate == [2,4]
-                if obj.path_metrics(2,4) == obj.path_metrics(1,3) + obj.paths3(2)
-                   currstate = [1,3];
-                   output(3) = 1;
-                else
-                    currstate = [3,3];  
-                    output(3) = 1;
-                end
-             elseif currstate == [3,4]
-                if obj.path_metrics(3,4) == obj.path_metrics(2,3) + obj.paths3(3)
-                   currstate = [2,3];
-                   output(3) = 0;
-                else
-                    currstate = [4,3];  
-                    output(3) = 0;
-                end 
-            elseif currstate == [4,4]
-                if obj.path_metrics(4,4) == obj.path_metrics(2,3) + obj.paths3(4)
-                   currstate = [2,3];
-                   output(3) = 1;
-                else
-                    currstate = [4,3];  
-                    output(3) = 1;
+            %Stage 3 - columns-2
+            for i=1:n_full_stages
+                if currstate == [1,obj.columns-i]
+                    if obj.path_metrics(1,obj.columns-i) == obj.path_metrics(1,obj.columns-1-i) + obj.paths_full(obj.columns-3-i,1)
+                       currstate = [1,obj.columns-1-i];
+                       output(obj.columns-1-i) = 0;
+                    else
+                        currstate = [3,obj.columns-1-i];  %if it didn't come from (1,3) then it came from (3,3)
+                        output(obj.columns-1-i) = 0;
+                    end
+                elseif currstate == [2,obj.columns-i]
+                    if obj.path_metrics(2,obj.columns-1) == obj.path_metrics(1,obj.columns-1-i) + obj.paths_full(obj.columns-3-i,2)
+                       currstate = [1,obj.columns-1-i];
+                       output(obj.columns-1-i) = 1;
+                    else
+                        currstate = [3,obj.columns-1-i];  
+                        output(obj.columns-1-i) = 1;
+                    end
+                 elseif currstate == [3,obj.columns-i]
+                    if obj.path_metrics(3,obj.columns-1) == obj.path_metrics(2,obj.columns-1-i) + obj.paths_full(obj.columns-3-i,3)
+                       currstate = [2,obj.columns-1-i];
+                       output(obj.columns-1-i) = 0;
+                    else
+                        currstate = [4,obj.columns-1-i];  
+                        output(obj.columns-1-i) = 0;
+                    end 
+                elseif currstate == [4,obj.columns-i]
+                    if obj.path_metrics(4,obj.columns-1) == obj.path_metrics(2,obj.columns-1-i) + obj.paths_full(obj.columns-3-i,4)
+                       currstate = [2,obj.columns-1-i];
+                       output(obj.columns-1-i) = 1;
+                    else
+                        currstate = [4,obj.columns-1-i];  
+                        output(obj.columns-1-i) = 1;
+                    end
                 end
             end
             
@@ -158,10 +168,16 @@ classdef VitDec < handle
         end
         
         function output = decode_data(obj, in)
-            fill_trellis(obj, in);
-            output = traceback(obj);
+		   [rows,columns]= size(in);
+		   obj.columns = columns;
+           obj.path_metrics = zeros(4, columns+1);
+           obj.paths_full = zeros(columns-4, 8);
+           
+           fill_trellis(obj, in);
+           output = traceback(obj);
         end
     end
+    
     methods (Static)
         function dis = hamming_distance(x,y)          
             dis = 0;
