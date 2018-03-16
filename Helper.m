ca6518a40c26c05bff5cd197f99c4e33f759b27f
floresjd <28540828+floresjd@users.noreply.github.com>
2018-03-15 20:47:16 -0700

    Added new functions
    dec2binvec (dec to be converted, length of vector returned)
       coverts the decimal to its vector representation
    binvec2binvec (vec, length of vec)
       coverts the vector to its dec representation
    get_mask_vec (len of the vector retunred, no of ones)
       returns a vector of decimial with the no of ones

--- Helper.m
+++ Helper.m
@@ -23,5 +23,44 @@ classdef Helper
             vec(2) = bitshift(bitand(num,bit2),-1);
             vec(3) = bitshift(bitand(num,bit3),-2);    
         end
+        function val = dec2binvec(i,len)
+            int = uint16(i);
+            one = uint16(1);
+            val = zeros(1,len);
+            for k = 1:len
+                index = bitshift(one,k-1);
+                if bitand(int,index) == index
+                    val(1,len - k + 1) = 1;
+                end
+            end
+        end
+        function val = binvec2dec(vec,len)
+            one = uint16(1);
+            val = uint16(0);
+            for k = 1:len
+                if uint16(vec(k)) == one
+                    val = bitor(val,bitshift(one,len - k));
+                end
+            end
+        end
+        function vec = get_mask_vec(len,n1s)
+            vec = zeros(1,len);
+            one = uint16(1);
+            count = 0;
+            for x = 1:1023
+                bin = uint16(x);
+                nOnes = 0;
+                for i = 1:10
+                    if bitand(one,bin) == one
+                        nOnes = nOnes + 1;
+                    end
+                    bin = bitshift(bin,-1);
+                end
+                if nOnes == n1s
+                    count = count + 1;
+                    vec(1,count) = uint16(x);
+                end
+            end
+        end
     end
 end
