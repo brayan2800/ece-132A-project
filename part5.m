29dc6ca99cf799ab5d8e208c08616323e0256543
floresjd <28540828+floresjd@users.noreply.github.com>
2018-03-16 16:41:11 -0700

    Added new outputs
    Now prints the minimum hamming distance between the codewords

--- part5.m
+++ part5.m
@@ -18,6 +18,26 @@ for i = 1:8
     encoded(i,1:10) = ftns.make_row(encoder.encode_data(inputs(i,1:5)));
 end
 
+%To get the minimum hamming distance
+first = true;
+d_H_min = 0;
+for k = 1:8
+    for j = 1:8
+        if j > k
+            local_d_H = ftns.hamming_distance(encoded(k,1:10),encoded(j,1:10),10);
+            if first
+                first = false;
+                d_H_min = local_d_H;
+            end
+            if local_d_H < d_H_min
+                d_H_min = local_d_H;
+            end
+        end
+    end
+end
+max_corrected = floor(d_H_min/2);
+
+
 corrupt_1_bits = uint16(zeros(80,1));
 corrupt_2_bits = uint16(zeros(360,1));
 corrupt_3_bits = uint16(zeros(960,1));
@@ -35,54 +55,64 @@ end
 % end
 
 
-correct = 0;
+correct1 = 0;
 for k = 1:8
     for j = 1:10
         corrupt = ftns.dec2binvec(corrupt_1_bits((k-1)*10+j,1),10);
         dec = decoder.decode_data(ftns.make_col(corrupt));
         if dec == inputs(k,1:5)
-            correct = correct + 1;
+            correct1 = correct1 + 1;
         end
     end
 end
-fprintf("----(Summary) Decoder decoded %d out of 80 inputs correctly\n", correct);
+
 
 for k = 1:8
     for j = 1:45
-        corrupt_2_bits((k-1)*45+j,1) = uint16(channel.flip_bits(encoded(i,1:10),B_flip_masks(1,j)));
+        corrupt_2_bits((k-1)*45+j,1) = uint16(channel.flip_bits(encoded(k,1:10),B_flip_masks(1,j)));
     end
 end
 
-correct = 0;
+correct2 = 0;
 for k = 1:8
     for j = 1:45
         corrupt = ftns.dec2binvec(corrupt_2_bits((k-1)*45+j,1),10);
+        col = ftns.make_col(corrupt);
         dec = decoder.decode_data(ftns.make_col(corrupt));
         if dec == inputs(k,1:5)
-            correct = correct + 1;
+            correct2 = correct2 + 1;
         end
     end
 end
-fprintf("----(Summary) Decoder decoded %d out of 360 inputs correctly\n", correct);
+
 
 
 for k = 1:8
     for j = 1:120
-        corrupt_3_bits((k-1)*120+j,1) = uint16(channel.flip_bits(encoded(i,1:10),C_flip_masks(1,j)));
+        corrupt_3_bits((k-1)*120+j,1) = uint16(channel.flip_bits(encoded(k,1:10),C_flip_masks(1,j)));
     end
 end
 
-correct = 0;
+correct3 = 0;
 for k = 1:8
     for j = 1:120
         corrupt = ftns.dec2binvec(corrupt_3_bits((k-1)*120+j,1),10);
         dec = decoder.decode_data(ftns.make_col(corrupt));
         if dec == inputs(k,1:5)
-            correct = correct + 1;
+            correct3 = correct3 + 1;
         end
     end
 end
-fprintf("----(Summary) Decoder decoded %d out of 960 inputs correctly\n", correct);
+
+
+fprintf("----(Summary) The minimun Hamming Distance is %d\n              So the max number of bits the decoder can correct is %d\n", d_H_min, max_corrected);
+fprintf("              Decoder decoded %d out of 80 inputs correctly\n", correct1);
+fprintf("              Decoder decoded %d out of 360 inputs correctly\n", correct2);
+fprintf("              Decoder decoded %d out of 960 inputs correctly\n", correct3);
+
+
+
+
 
 
 
