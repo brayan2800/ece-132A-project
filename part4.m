53e5b3b0a498c5a39718bda8e2fe29a1f55cc55b
floresjd <28540828+floresjd@users.noreply.github.com>
2018-03-16 18:55:09 -0700

    Commented
    Comments to better explain our goals

--- part4.m
+++ part4.m
@@ -1,20 +1,26 @@
 %This script is for part 4 of the project spec
 %Test out code to show that for the 8 generated outputs the decoder
 %produces the correct three input bits (plus two zeros)
-ftns = Helper;
-inputs = zeros(8,5);
+
+ftns = Helper; %a list of useful functions used to simply this script
+
+inputs = zeros(8,5); %a matrix of the 8 possible inputs
+
+%generate the inputs rather than hard coding each one
 for i = 1:8
     inputs(i,1:3) = ftns.input_vector(i-1);
 end
 
-encoder = ConvEnc;
-decoder = VitDec;
-channel = Channel;
+encoder = ConvEnc;% the convolutional encoder
+decoder = VitDec;% the hard decoding Viterbi decoder
 
 correct = 0;
-encoded = zeros(8,10);
-decoded = zeros(8,5);
-for i = 1:8
+encoded = zeros(8,10); % a matrix of the encoded codewords
+decoded = zeros(8,5); % a matrix of decoded codewords
+
+%for every row in inputs encode
+%   decode and compare to input
+for i = 1:8 
     input = inputs(i,1:5);
     encode = encoder.encode_data(input);
     encoded(i,1:10) = ftns.make_row(encode);
